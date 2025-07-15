b9d74f38f188cd48539f1da00124f08f844f682d
KarlaJMT <2302091@utrivieramaya.edu.mx>
2025-07-15 10:57:13 -0500

    subirdato

---
+++ src/context/contextProductos.jsx
@@ -0,0 +1,31 @@
+import React, { createContext, useState } from 'react';
+import { View, Text } from 'react-native'
+import { collection, addDoc } from 'firebase/firestore'
+import { db } from '../../src/script/configFirebase';
+
+export const estadoGlobalProductos = createContext();
+
+const subirdato = async () => {
+    try {
+      const docRef = await addDoc(collection(db, 'productos'), {
+        nombre: 'chocolate',
+        cantidad: 2,
+        fecha: new Date(),
+      })
+      console.log('Documento creado con el ID', docRef.id)
+    } catch (error) {
+      console.log('Error detectado.')
+    }
+  }
+
+
+
+export default function contextProductos({ children }) {
+
+    const [ dataproductos, setDataproductos ] = useState([])
+    return (
+        <estadoGlobalProductos.Provider value={{}}>
+            { children }
+        </estadoGlobalProductos.Provider>
+    )
+}
--- src/screen/home/ScreenHome.jsx
+++ src/screen/home/ScreenHome.jsx
@@ -1,24 +1,10 @@
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
-import { collection, addDoc } from 'firebase/firestore';
-import { db } from '../../script/configFirebase';
 import { Button } from 'react-native-web';
 
 export default function ScreenHome() {
 
-  const subirdato = async () => {
-    try {
-      const docRef = await addDoc(collection(db, 'productos'), {
-        nombre: 'chocolate',
-        cantidad: 2,
-        fecha: new Date,
-      })
-      console.log('Documento creado con el ID', docRef.id)
-    } catch (error) {
-      console.log('Error detectado.')
-    }
-
-  }
+  
 
 
   return (
